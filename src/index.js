dfa4d535980ca7c981d960ba7260758087940d99
SergeRomanenko13 <serhiiromanenko13@gmail.com>
2021-07-20 12:40:19 +0300

    1st commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+// 'use strict';
+/*global $*/
+
+const accordionData = [
+    {
+        title: 'London',
+        content: `London is the capital and largest city of England and the United Kingdom.
+        The city stands on the River Thames in the south-east of England,
+        at the head of its 50-mile (80 km) estuary leading to the North Sea.`,
+    },
+    {
+        title: 'Madrid',
+        content: `Madrid is the capital and most-populous city of Spain. The city has almost 3.4 million
+        inhabitants and a metropolitan area population of approximately 6.7 million.
+        It is the second-largest city in the European Union, surpassed only by Berlin in its administrative limits.`,
+    },
+    {
+        title: 'Paris',
+        content: `Paris is the capital and most populous city of France, with an estimated population of
+        2,175,601 residents as of 2018, in an area of more than 105 square kilometres (41 square miles).`,
+    },
+    {
+        title: 'Barcelona',
+        content: `Barcelona is a city on the coast of northeastern Spain. It is the capital and largest city
+        of the autonomous community of Catalonia, as well as the second most populous municipality of Spain.`,
+    },
+    {
+        title: 'Milan',
+        content: `Milan is a city in northern Italy, capital of Lombardy, and the second-most populous city proper
+        in Italy after Rome. It is the only European city wealthier than its corresponding capital.`,
+    },
+];
+
+$(accordionData).map((index, item) => {
+    const $title = $('<h2>');
+    $title.text(item.title).css('text-align', 'center');
+    $('#section').append($title);
+
+    const $info = $('<p>');
+    $info.text(item.content);
+    $('#section').append($info);
+    $info.hide();
+
+    //------------------STYLE--------------//
+    $info.css({
+        'text-align': 'justify',
+        'padding': '0 50px',
+    });
+    $title.css({
+        'border-bottom': '2px solid rgb(193, 209, 217',
+        'cursor': 'pointer',
+    });
+
+    // console.log($info.html());
+
+    $title.on('click', () => {
+        // console.log($info.text());
+
+        if ($info.slideUp()) {
+            $info.slideDown();
+        }
+    });
+});
+
+//--------------------STYLES IN JQUERY-------------------//
+
+$('body').css('background-color', '#BBCBD8');
+
+$('section').css({
+    'font-size': '15px',
+    'background-color': 'white',
+    'width': '400px',
+    'margin': '0 auto',
+    'padding-top': '1px',
+    'color': '#6F7A82',
+});
